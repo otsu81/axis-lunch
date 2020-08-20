924f2ed032499dbf8f310e6675ccfc3240c2aa4f
otsu81 <c@otsu.se>
2020-08-20 12:49:49 +0200

    refactor fetching restaurant URLs

--- scraper/main.py
+++ scraper/main.py
@@ -1,31 +1,36 @@
 import os
 import json
 import logging
+from csv import DictReader
 from restaurants.paolos import Paolos
 from restaurants.pieplow import Pieplow
 from restaurants.edison import Edison
 from ddb import RestaurantTable
 
-logging.basicConfig(level=os.environ.get('LOGLEVEL'))
+logging.basicConfig(level=os.environ.get('LOGLEVEL', 'WARNING'))
 log = logging.getLogger('scraper')
 
 
 def handler(event, context):
 
+    # define all restaurants to be parsed and generated
     classmap = {
         'Paolos': Paolos(),
         'Edison': Edison(),
         'Pieplow': Pieplow()
     }
 
+    # get URLs for respective restaurants
     restaurants = dict()
-    with open('restaurants.csv', 'r') as f:
-        for r in f.readlines():
-            info = r.rstrip().split(',')
-            restaurants[info[0]] = \
-                classmap[info[0]].get_week_menu(info[1])
-            restaurants[info[0]]['url'] = info[1]
-    ddb = RestaurantTable()
+    with open('restaurants_sandbox.csv', 'r') as f:
+        reader = DictReader(f)
+        for row in reader:
+            restaurants[row['restaurant']]['url'] = row['url']
+            restaurants[row['restaurant']] = \
+                classmap[row['restaurant']].get_week_menu(row['url'])
+
+    # update the DDB table with restaurant menus
+    ddb = RestaurantTable(table_name=os.environ.get('TABLE_NAME'))
     for r in restaurants:
         ddb.update_restaurant_item(
             r, restaurants[r]
@@ -33,6 +38,7 @@ def handler(event, context):
 
     log.info(json.dumps(restaurants, indent=4, default=str))
 
+    # return list of attempted restaurants
     return {
             'success': 'true',
             'restaurants': list(restaurants.keys())
---
+++ scraper/restaurants/__init__.py
@@ -0,0 +1,3 @@
+from .paolos import Paolos
+from .edison import Edison
+from .pieplow import Pieplow
