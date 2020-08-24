9250e0997fb53ebd088d2e3400cbfbf69504f2d7
otsu81 <c@otsu.se>
2020-08-24 14:49:35 +0200

    fixed pieplow broken parse

--- bucket_cleanup/main.py
+++ bucket_cleanup/main.py
@@ -1,8 +1,7 @@
-import json
 import boto3
 import logging
+import cfnresponse
 import os
-from botocore.vendored import requests
 
 
 log = logging.getLogger('bucket_cleanup')
@@ -12,29 +11,15 @@ log.setLevel(os.environ.get('LOGLEVEL', 'WARNING'))
 def handler(event, context):
     try:
         bucket = event['ResourceProperties']['BucketName']
-
+        log.info(
+            f"bucket: {bucket}, event['RequestType']: {event['RequestType']}"
+        )
         if event['RequestType'] == 'Delete':
             s3 = boto3.resource('s3')
             bucket = s3.Bucket(bucket)
-            for obj in bucket.objects.filter():
-                s3.Object(bucket.name, obj.key).delete()
+            bucket.objects.all().delete()
 
-        sendResponseCfn(event, context, "SUCCESS")
+        cfnresponse.send(event, context, cfnresponse.SUCCESS, {})
     except Exception as e:
-        log.warning(e)
-        sendResponseCfn(event, context, "FAILED")
-
-
-def sendResponseCfn(event, context, responseStatus):
-    response_body = {
-        'Status': responseStatus,
-        'Reason': 'Log stream name: ' + context.log_stream_name,
-        'PhysicalResourceId': context.log_stream_name,
-        'StackId': event['StackId'],
-        'RequestId': event['RequestId'],
-        'LogicalResourceId': event['LogicalResourceId'],
-        'Data': json.loads("{}")
-    }
-
-    requests.put(event['ResponseURL'], data=json.dumps(response_body))
-    return response_body
+        log.warning(f"Signaling failure to CloudFormation: {e}")
+        cfnresponse.send(event, context, cfnresponse.FAILED, {})
