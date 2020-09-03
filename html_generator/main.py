b2c090e423c9a1d9d9d0ff2ab93f3b1b6a4c477e
otsu81 <c@otsu.se>
2020-09-03 15:14:34 +0200

    change variable name for DDB GSI

--- html_generator/main.py
+++ html_generator/main.py
@@ -17,7 +17,7 @@ def get_restaurant_menus_dict(restaurants):
     menus = dict()
     for r in restaurants:
         result = table.query(
-            IndexName='sortByDate',
+            IndexName='sortByDate2',
             ConsistentRead=False,
             ScanIndexForward=False,
             Limit=1,
